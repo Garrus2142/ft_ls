dffcde7de15f65e28bf3f9d2125456a4085b457a
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-02-14 16:55:08 +0100

    Support follow link if no long option

--- src/stats.c
+++ src/stats.c
@@ -6,7 +6,7 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 09:05:09 by thugo             #+#    #+#             */
-/*   Updated: 2017/02/03 09:57:48 by thugo            ###   ########.fr       */
+/*   Updated: 2017/02/14 14:05:47 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@
 #include <string.h>
 #include "ft_ls.h"
 
-void		file_get_stats(t_file *file)
+void		file_get_stats(t_params *p, t_file *file)
 {
 	int	ret;
 
-	if (lstat(file->path, &(file->stats)) != 0)
-		file->errmsg = strerror(errno);
-	if ((ret = listxattr(file->path, NULL,0, XATTR_NOFOLLOW)) == -1)
+	if (p->options & OPT_l)
+	{
+		if (lstat(file->path, &(file->stats)) != 0)
+			file->errmsg = strerror(errno);
+	}
+	else
+	{
+		if (stat(file->path, &(file->stats)) != 0)
+			file->errmsg = strerror(errno);
+	}
+	if ((ret = listxattr(file->path, NULL, 0, XATTR_NOFOLLOW)) == -1)
 		file->errmsg = strerror(errno);
 	file->xattr = ret > 0 ? 1 : 0;
 }
