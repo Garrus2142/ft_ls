a170579438298058ad03f3cf97dafc8d367e686d
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-02-18 18:45:49 +0100

    Show info about link if target no exist

--- src/stats.c
+++ src/stats.c
@@ -6,7 +6,7 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 09:05:09 by thugo             #+#    #+#             */
-/*   Updated: 2017/02/18 15:50:41 by thugo            ###   ########.fr       */
+/*   Updated: 2017/02/18 18:44:26 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,15 @@ void		file_get_stats(t_params *p, t_file *file)
 
 	if (!(p->options & OPT_L_LOW) && file->infos & IS_OPERAND)
 	{
-		if (stat(file->path, &(file->stats)) != 0 && !(file->infos & IS_ERROR))
-			ls_error(file);
+		if (stat(file->path, &(file->stats)))
+		{
+			if (lstat(file->path, &(file->stats)) && !(file->infos & IS_ERROR))
+				ls_error(file);
+		}
 	}
 	else
 	{
-		if (lstat(file->path, &(file->stats)) != 0 && !(file->infos & IS_ERROR))
+		if (lstat(file->path, &(file->stats)) && !(file->infos & IS_ERROR))
 			ls_error(file);
 	}
 	if ((ret = listxattr(file->path, NULL, 0, XATTR_NOFOLLOW)) > 0)
