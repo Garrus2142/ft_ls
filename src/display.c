9326e5071221106893bb52e15a823850905f08ff
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-02-18 16:35:05 +0100

    Only -1 output WIP

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 12:55:13 by thugo             #+#    #+#             */
-/*   Updated: 2017/02/17 21:16:31 by thugo            ###   ########.fr       */
+/*   Updated: 2017/02/18 16:33:05 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@
 void	display_file(t_params *p, t_file *file)
 {
 	t_list	*cur;
-	ft_printf("\n%s:\n", file->path);
+
+	if (!(file->stats.st_mode & S_IFDIR))
+		ft_printf("%s\n", file->path);
+	else if (!(file->infos & IS_OPERAND) || p->nfiles > 1)
+		ft_printf("%s%s:\n", (file->infos & IS_FIRST) ? "" : "\n", file->path);
 	cur = file->childs;
+	//ft_printf("Total: %llu\n", file->total);
 	while (cur)
 	{
-		ft_printf("%s\n", ((t_file *)cur->content)->name);
+		ft_printf("%s\n", ACC_FILE(cur)->name);
 		cur = cur->next;
 	}
 }
