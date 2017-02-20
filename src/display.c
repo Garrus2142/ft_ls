ae71d66be109dd53bdfff2bef39ab870df9b9b83
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-02-20 06:16:11 +0100

    Add support color, fix field width with /dev. Bad norme !

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 12:55:13 by thugo             #+#    #+#             */
-/*   Updated: 2017/02/19 18:17:33 by thugo            ###   ########.fr       */
+/*   Updated: 2017/02/20 06:07:13 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,72 @@
 // Si file type == Block special or Charactere special, afficher le minor et major a la place de size
 // Si c'est un liens alors on affiche la cible -> 
 // Voir les cas des other permission (x, S, t)
+// Si operand & long format afficher le path
 
-static void	print_long(t_params *p, t_file *par, t_file *file)
+static char	*color(t_params *p, t_file *file)
 {
-	char	rights[12];
-	char	*pwname;
-	char	*grname;
+	static char color[10];
 
-	if (!(pwname = file->pw_name ? file->pw_name : ft_itoa(file->stats.st_uid)))
-		exit(EXIT_FAILURE);
-	if (!(grname = file->gr_name ? file->gr_name : ft_itoa(file->stats.st_gid)))
-		exit(EXIT_FAILURE);
-	get_rights(p, file, rights);
-	ft_printf("%-11s %*lu %-*s  %-*s", rights, par->nlink_max, file->stats.st_nlink,
-		 par->n_pw_name, pwname, par->n_gr_name, grname, par->size_max);
+	if (!(p->options & OPT_G))
+		return (ft_strcpy(color, "0m"));
+	if (S_ISDIR(file->stats.st_mode))
+		return (ft_strcpy(color, "1m"));
+	else if (S_ISLNK(file->stats.st_mode))
+		return (ft_strcpy(color, "35m"));
+	else if (S_ISSOCK(file->stats.st_mode))
+		return (ft_strcpy(color, "32m"));
+	else if (S_ISFIFO(file->stats.st_mode))
+		return (ft_strcpy(color, "33m"));
+	else if (file->stats.st_mode & S_ISUID && file->stats.st_mode &
+			(S_IXUSR | S_IXGRP | S_IXOTH))
+		return (ft_strcpy(color, "30;41m"));
+	else if (file->stats.st_mode & S_ISGID && file->stats.st_mode &
+			(S_IXUSR | S_IXGRP | S_IXOTH))
+		return (ft_strcpy(color, "30;46m"));
+	else if (file->stats.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))
+		return (ft_strcpy(color, "31m"));
+	else if (S_ISBLK(file->stats.st_mode))
+		return (ft_strcpy(color, "34;46m"));
+	else if (S_ISCHR(file->stats.st_mode))
+		return (ft_strcpy(color, "34;43m"));
+	return (ft_strcpy(color, "0m"));
+}
+
+static void	print_long2(t_params *p, t_file *par, t_file *file)
+{
 	if (S_ISBLK(file->stats.st_mode) || S_ISCHR(file->stats.st_mode))
 	{
 		ft_printf("  %3lu, %3lu", major(file->stats.st_rdev),
 			minor(file->stats.st_rdev));
 	}
 	else
-		ft_printf("  %*lu", par->size_max + ((par->infos & HAS_BLKCHR) * 7), file->stats.st_size);
-	ft_printf(" %s\n", file->name);
-	free(pwname);
-	free(grname);
+	{
+		ft_printf("  %*lu", ft_max(par->size_max, ((par->infos & HAS_BLKCHR)
+			* 8)), file->stats.st_size);
+	}
+	ft_printf(" %s \033[%s%s\033[0m", file->mtime, color(p, file),
+		(file->infos & IS_OPERAND ? file->path : file->name));
+	if (S_ISLNK(file->stats.st_mode))
+		ft_printf(" -> %s", file->linktarget);
+	ft_putchar('\n');
+}
+
+static void	print_long(t_params *p, t_file *par, t_file *file)
+{
+	char	rights[12];
+	char	*pw;
+	char	*gr;
+
+	if (!(pw = file->pw_name ? file->pw_name : ft_itoa(file->stats.st_uid)))
+		exit(EXIT_FAILURE);
+	if (!(gr = file->gr_name ? file->gr_name : ft_itoa(file->stats.st_gid)))
+		exit(EXIT_FAILURE);
+	get_rights(p, file, rights);
+	ft_printf("%-11s %*lu %-*s  %-*s", rights, par->nlink_max, file->stats.
+		st_nlink, par->n_pw_name, pw, par->n_gr_name, gr, par->size_max);
+	free(pw);
+	free(gr);
+	print_long2(p, par, file);
 }
 
 void	display_file(t_params *p, t_file *file)
@@ -49,7 +91,7 @@ void	display_file(t_params *p, t_file *file)
 	t_list	*cur;
 
 	if (!(file->stats.st_mode & S_IFDIR) && !(p->options & OPT_L_LOW))
-		ft_printf("%s\n", file->path);
+		ft_printf("\033[%s%s\033[0m\n", color(p, file), file->path);
 	else if (!(file->infos & IS_OPERAND) || p->nfiles > 1)
 		ft_printf("%s%s:\n", (file->infos & IS_FIRST) ? "" : "\n", file->path);
 	if (p->options & OPT_L_LOW)
@@ -58,16 +100,16 @@ void	display_file(t_params *p, t_file *file)
 		file->size_max = ft_sizelong(file->size_max);
 		if (file->infos & IS_OPERAND && !(S_ISDIR(file->stats.st_mode)))
 			print_long(p, file, file);
+		else
+			ft_printf("total: %lu\n", file->total);
 	}
 	cur = file->childs;
-	//ft_printf("Total: %llu\n", file->total);
-	//ft_printf("nlink_max: %lu\n", file->nlink_max);
 	while (cur)
 	{
 		if (p->options & OPT_L_LOW)
 			print_long(p, file, ACC_FILE(cur));
 		else
-			ft_printf("%s\n", ACC_FILE(cur)->name);
+			ft_printf("\033[%s%s\033[0m\n", color(p, ACC_FILE(cur)), ACC_FILE(cur)->name);
 		cur = cur->next;
 	}
 }
